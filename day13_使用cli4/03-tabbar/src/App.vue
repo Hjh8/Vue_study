3e0ae9480f5a78c1f779de6b54b7a31b76d511eb
Hjh8 <50100494+Hjh8@users.noreply.github.com>
2020-02-21 21:41:18 +0800

    第8章学习，tabbar案例

---
+++ day13_使用cli4/03-tabbar/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <TabBar>
+      <tab_item path="/home">
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg">
+        <div slot="item-text">首页</div>
+      </tab_item>
+      <tab_item path='/category'>
+        <img slot="item-icon" src="./assets/img/tabbar/category.svg">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg">
+        <div slot="item-text">分类</div>
+      </tab_item>
+      <tab_item path='/cart'>
+        <img slot="item-icon" src="./assets/img/tabbar/cart.svg">
+        <img slot="item-icon-active" src="./assets/img/tabbar/cart_active.svg">
+        <div slot="item-text">购物车</div>
+      </tab_item>
+      <tab_item path='/profile'>
+        <img slot="item-icon" src="./assets/img/tabbar/profile.svg">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg">
+        <div slot="item-text">我的</div>
+      </tab_item>
+    </TabBar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import tab_item from './components/tabbar/tab-item'
+
+export default {
+  components:{
+    TabBar,
+    tab_item
+  }
+}
+</script>
+ 
+<style>
+  @import url(./assets/css/base.css); 
+  
+</style>
---
+++ day13_使用cli4/03-tabbar/src/components/tabbar/tab-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class='tab-item' @click="itemclick">
+    <!-- 在slot外面包装一层div是防止有些样式被替换掉 -->
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{itemActive: isActive}"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    path:String
+  },
+  data(){
+    return{
+      isActive:false
+    }
+  },
+  methods:{
+    itemclick(){
+      this.$router.replace(this.path);
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+
+<style>
+  .tab-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .tab-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+  .itemActive{
+    color: red;
+  }
+</style>
